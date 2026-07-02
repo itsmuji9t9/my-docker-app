eab8d344f172a38460969f415e7bf4fef481efa2
Hacker <jutt7575248@gmail.com~>
2026-07-02 17:06:14 +0500

    this is my first docker app

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# 1. Base image select karein jismein Python pehle se install ho
+FROM python:3.10-slim
+
+# 2. Container ke andar ek folder banayein jahan kaam hoga
+WORKDIR /app
+
+# 3. Apne computer se app.py file ko container ke andar copy karein
+COPY app.py .
+
+# 4. Container ka port 8080 open karein taake internet se traffic aa sake
+EXPOSE 80
+
+# 5. Container start hote hi jo command chalani hai
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 80
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        # Mujahid, aap yahan apna custom message dekh sakte hain
+        self.wfile.write(b"<h1>Hello this is Mujahid! Congratulations Python App is running inside Docker! \ud83d\ude80</h1>")
+
+print(f"Server starting on port {PORT}...")
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    httpd.serve_forever()
