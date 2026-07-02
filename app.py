7ce4e42b692ce4d0e9923fc504b5531b1539d031
Hacker <jutt7575248@gmail.com~>
2026-07-02 19:55:13 +0500

    small text cahnges to app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         # Mujahid, aap yahan apna custom message dekh sakte hain
         self.wfile.write(b"<h1>Hello this is Mujahid! Congratulations Python App is running inside Docker! \ud83d\ude80</h1>")
+        self.wfile.write(b"<h1>This is my first ci cd pipeline setup  \ud83d\ude80</h1>")
 
 print(f"Server starting on port {PORT}...")
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
